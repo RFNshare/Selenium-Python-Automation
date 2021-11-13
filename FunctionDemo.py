31265d507b836cddfbd2efcc4e7409a505bf91ce
rfnshare <rfnshare@gmail.com>
2021-11-13 11:18:26 +0600

    Function concept Updated

---
+++ FunctionDemo.py
@@ -0,0 +1,14 @@
+def greet_me(name):
+    print("Welcome, Agent " + name)
+
+
+greet_me("Faroque")
+greet_me("Abdul")
+
+
+def add_two_int(a, b):
+    return a + b
+
+
+a = add_two_int(1, 2)
+print(a)
