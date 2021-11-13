df7109e7899cbcda517099bf13c08e7bc251ba76
rfnshare <rfnshare@gmail.com>
2021-11-13 18:08:56 +0600

    Selenium Basic ++ locators

--- pytestsSelenium/DemoBrowser.py
+++ pytestsSelenium/DemoBrowser.py
@@ -1,34 +1,34 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-
-driver = webdriver.Chrome('./chromedriver.exe')
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver = webdriver.Firefox(
-#     executable_path=r'C:\Users\rfnsh\PycharmProjects\PythonTestingUdemy\pytestsSelenium\geckodriver.exe')
-# driver = webdriver.Edge(
-#     executable_path=r'C:\Users\rfnsh\PycharmProjects\PythonTestingUdemy\pytestsSelenium\msedgedriver.exe')
-# driver.maximize_window()
-# driver.get("https://rfnshare.github.io")
-# print(driver.title)
-# print(driver.current_url)
-# driver.get("https://www.fiverr.com/rfnshare")
-# driver.back()
-
-driver.get("https://rahulshettyacademy.com/angularpractice")
-driver.refresh()
-# input fields
-name = driver.find_element_by_xpath('/html/body/app-root/form-comp/div/form/div[1]/input')
-name.send_keys("Abdullah Al Faroque")
-email = driver.find_element_by_xpath('/html/body/app-root/form-comp/div/form/div[2]/input')
-email.send_keys('demoemail@gmail.com')
-password = driver.find_element_by_xpath('//*[@id="exampleInputPassword1"]')
-password.send_keys('123456')
-# checkbox
-check = driver.find_element_by_xpath('//*[@id="exampleCheck1"]')
-time.sleep(2)
-check.click()
-
-# driver.minimize_window()
-# driver.close()  # current window close
-# driver_c.quit()  # all window close
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# import time
+#
+# driver = webdriver.Chrome('./chromedriver.exe')
+# # driver = webdriver.Chrome(ChromeDriverManager().install())
+# # driver = webdriver.Firefox(
+# #     executable_path=r'C:\Users\rfnsh\PycharmProjects\PythonTestingUdemy\pytestsSelenium\geckodriver.exe')
+# # driver = webdriver.Edge(
+# #     executable_path=r'C:\Users\rfnsh\PycharmProjects\PythonTestingUdemy\pytestsSelenium\msedgedriver.exe')
+# # driver.maximize_window()
+# # driver.get("https://rfnshare.github.io")
+# # print(driver.title)
+# # print(driver.current_url)
+# # driver.get("https://www.fiverr.com/rfnshare")
+# # driver.back()
+#
+# driver.get("https://rahulshettyacademy.com/angularpractice")
+# driver.refresh()
+# # input fields
+# name = driver.find_element_by_xpath('/html/body/app-root/form-comp/div/form/div[1]/input')
+# name.send_keys("Abdullah Al Faroque")
+# email = driver.find_element_by_xpath('/html/body/app-root/form-comp/div/form/div[2]/input')
+# email.send_keys('demoemail@gmail.com')
+# password = driver.find_element_by_xpath('//*[@id="exampleInputPassword1"]')
+# password.send_keys('123456')
+# # checkbox
+# check = driver.find_element_by_xpath('//*[@id="exampleCheck1"]')
+# time.sleep(2)
+# check.click()
+#
+# # driver.minimize_window()
+# # driver.close()  # current window close
+# # driver_c.quit()  # all window close
---
+++ pytestsSelenium/demo_browser.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+import time
+
+from selenium.webdriver.support.select import Select
+
+# driver = webdriver.Chrome('./chromedriver.exe')
+driver = webdriver.Firefox(
+executable_path=r'C:\Users\rfnsh\PycharmProjects\PythonTestingUdemy\pytestsSelenium\geckodriver.exe')
+
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/angularpractice")
+
+driver.refresh()
+
+# input fields
+name = driver.find_element_by_xpath('/html/body/app-root/form-comp/div/form/div[1]/input')
+name.clear()
+name.send_keys("Abdullah Al Faroque")
+email = driver.find_element_by_xpath('/html/body/app-root/form-comp/div/form/div[2]/input')
+email.send_keys('demoemail@gmail.com')
+password = driver.find_element_by_xpath('//*[@id="exampleInputPassword1"]')
+password.send_keys('123456')
+# checkbox
+check = driver.find_element_by_xpath('//*[@id="exampleCheck1"]')
+time.sleep(2)
+check.click()
+# dropdown
+gender = Select(driver.find_element_by_xpath('//*[@id="exampleFormControlSelect1"]'))
+gender.select_by_visible_text("Female")
+gender.select_by_index(0)
+# radio
+driver.find_element_by_xpath('//*[@id="inlineRadio1"]').click()
+# gender.select_by_value("F")
+# submit
+driver.find_element_by_xpath('/html/body/app-root/form-comp/div/form/input').click()
+# success message grab
+display = driver.find_element_by_xpath('/html/body/app-root/form-comp/div/div[2]/div').text
+print(display)
+assert "Success" in display
+# extra locator
+# driver.find_element_by_link_text('')
