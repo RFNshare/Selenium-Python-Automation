39caff08615a8f99ccfc717e4a1881ce4aee4d35
rfnshare <rfnshare@gmail.com>
2021-11-13 09:59:20 +0600

    First Demo Updated

---
+++ FirstDemo.py
@@ -0,0 +1,15 @@
+import main
+
+print("Hello World")
+main.print_hi('PyCharm Hi')
+
+# This is a comment
+a = 3
+print(a)
+
+b, c, d = 5, 9 / 7, 'ABC'
+print("Value is b =", b)
+print("{} {}".format("Value is", b))
+print(type(b))
+print(type(c))
+print(type(d))
