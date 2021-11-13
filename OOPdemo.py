cdaa646873c4c8d3145642296e0b6279541201c2
rfnshare <rfnshare@gmail.com>
2021-11-13 11:47:31 +0600

    OOP concept(Class, Object, Method, Constructor, Class Variable, Instance Variable Updated

---
+++ OOPdemo.py
@@ -0,0 +1,21 @@
+class Calculator:  # Class
+    num = 100  # Class Variable
+
+    def __init__(self, a, b):  # Constructor
+        self.a = a  # Instance Variable
+        self.b = b
+        print("This is a Constructor Method in Calculator class")
+
+    def data(self):  # Method
+        print("This is a Data Method in Calculator Class")
+
+    def Summation(self):
+        return self.a + self.b + self.num
+
+
+obj = Calculator(1, 2)  # Create Object
+print(obj.Summation())
+
+obj1 = Calculator(2, 2)  # Create Another Object
+print(obj1.Summation())
+
