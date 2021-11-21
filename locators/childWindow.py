011c9ab70ead7cbf0344735093319d55380ab162
rfnshare <rfnshare@gmail.com>
2021-11-21 15:27:38 +0600

    Parent, Child Window

--- locators/childWindow.py
+++ locators/childWindow.py
@@ -9,4 +9,13 @@ driver = webdriver.Chrome(
     executable_path=r'C:\Users\rfnsh\PycharmProjects\PythonTestingUdemy\pytestsSelenium\chromedriver.exe')
 driver.maximize_window()
 # driver.implicitly_wait(5)
-driver.get("https://the-internet.herokupp.com/windows")
+driver.get("https://the-internet.herokuapp.com/windows")
+
+driver.find_element_by_link_text("Click Here").click()
+child_window = driver.window_handles[1]
+driver.switch_to.window(child_window)
+
+print(driver.find_element_by_tag_name("h3").text)
+driver.close()
+driver.switch_to.window(driver.window_handles[0])
+print(driver.find_element_by_tag_name("h3").text)
